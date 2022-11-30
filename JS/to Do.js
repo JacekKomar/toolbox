523acb2535389676183af7969598010e09410f33
JacekKomar <jacek.komar2000@gmail.com>
2022-11-30 15:41:22 +0100

    Add bootstrap

---
+++ JS/to Do.js
@@ -0,0 +1,17 @@
+/*
+kalendarz 
+data i godz aktualne
+kalkulator
+
+PLAN DNIA
+losowe wywanie np sportowe żeby strona losowała
+
+cytat dnia
+ciemne i jasne tło do wyboru
+dwa języki 
+logowanie może
+
+
+
+
+*/
