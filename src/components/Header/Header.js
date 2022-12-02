e182b6b2639367ffb454a31019b9fed1fa63ecc0
JacekKomar <jacek.komar2000@gmail.com>
2022-12-02 13:37:53 +0100

    Add brightened and description in all items in main menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,37 +16,56 @@ export default class Header extends React.Component {
 
         <div className="row">
           <div className="mainPage">
-            <div className="displayofHeaders">
-              <div className="ChooseSite col-3 p-4 border border-dark rounded">
-                <h1 className="nameParagraf">Wybierz: Kalkulator</h1>
+            <div className="displayofHeaders p-2">
+              <div className="ChooseSite col-3 p-4 mr-1 border border-dark rounded brightened">
+                <h1 className="nameParagraf text-center">
+                  Wybierz: Kalkulator
+                </h1>
                 <img className="img-fluid" src={calculatorIcon}></img>
-                <p className="descriptionOfLinks"> Kalkulator</p>
+                <p className="descriptionOfLinks text-center">
+                  {" "}
+                  Kalkulator to urządzenie, które wyręczy cię od wykonywania
+                  obliczeń matematycznych.
+                </p>
 
                 <Link to="./components/calculator/calculator">Kalkulator</Link>
               </div>
 
-              <div className="ChooseSite col-3 p-4 border border-dark rounded">
-                <h1 className="nameParagraf">Wybierz: Kalendaż</h1>
+              <div className="ChooseSite col-3 p-4 mr-1 border border-dark rounded brightened">
+                <h1 className="nameParagraf text-center">Wybierz: Kalendaż</h1>
                 <img className="img-fluid" src={calendarIcon}></img>
-                <p className="descriptionOfLinks"> kalendaż</p>
+                <p className="descriptionOfLinks text-center">
+                  kalendaż pozwala przejżeć poszczególne dni i dodać do nich
+                  wydarzenia.
+                </p>
 
-                <Link to="/calendar">Kalendaż</Link>
+                <Link to="/calendar ">Kalendaż</Link>
               </div>
 
-              <div className="ChooseSite col-3 p-4 border border-dark rounded">
-                <h1 className="nameParagraf">Wybierz: Stwórz plan dnia</h1>
+              <div className="ChooseSite col-3 p-4 mr-1 border border-dark rounded brightened">
+                <h1 className="nameParagraf text-center">Wybierz: Plan dnia</h1>
                 <img className="img-fluid" src={dayPlanIcon} />
-                <p className="descriptionOfLinks"> Plan dnia</p>
+                <p className="descriptionOfLinks text-center">
+                  {" "}
+                  to tutaj możesz ułożyć swój indywidualny i dostosowany do
+                  swoich potrzeb plan dnia.
+                </p>
 
                 <Link to="/planDay">Plan dnia</Link>
               </div>
 
-              <div className="ChooseSite col-3 p-4 border border-dark rounded">
-                <h1 className="nameParagraf">Wybierz: Stwórz plan dnia</h1>
+              <div className="ChooseSite col-3 mr-1 p-4 border border-dark rounded brightened">
+                <h1 className="nameParagraf text-center">
+                  Wybierz: Aktualna pogoda
+                </h1>
                 <img className="img-fluid" src={weatherIcon} />
-                <p className="descriptionOfLinks"> Plan dnia</p>
+                <p className="descriptionOfLinks text-center">
+                  {" "}
+                  Sprawdź aktualną pogodę panującą w dowolnym miejscu na
+                  świecie.
+                </p>
 
-                <Link to="/planDay">Plan dnia</Link>
+                <Link to="/planDay"></Link>
               </div>
             </div>
           </div>
