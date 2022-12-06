c9abde7de69bac344d308c7c167c4beb4e800b75
JacekKomar <jacek.komar2000@gmail.com>
2022-12-06 17:10:52 +0100

    Add buttons to calculator

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/calculator.scss";
+
 export default class Calculator extends React.Component {
   render() {
     return (
@@ -9,6 +10,30 @@ export default class Calculator extends React.Component {
         </div>
         <div className="calculatorTable rounded border border-dark mt-2">
           tabela
+          <div className="calculator-grid">
+            <div className="output">
+              <div className="previous-operand"></div>
+              <div className="current-operant"></div>
+            </div>
+            <button className="span-two">AC</button>
+            <button>DEL</button>
+            <button>/</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>*</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button className="span-two">=</button>
+          </div>
         </div>
       </div>
     );
