bd2ebdcb7b857c54c89d91ca24185f86946c0246
JacekKomar <jacek.komar2000@gmail.com>
2022-12-07 12:36:39 +0100

    Operation with noumbers start working

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -24,7 +24,51 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let Computation = "";
+  switch (operation) {
+    case "+":
+      Computation = prev + current;
+      break;
+    case "-":
+      Computation = prev - current;
+      break;
+    case "*":
+      Computation = prev * current;
+      break;
+    case "/":
+      Computation = prev / current;
+      break;
+  }
+  return Computation.toString();
 }
 
 function Calculator() {
@@ -48,21 +92,26 @@ function Calculator() {
             </div>
             <div className="current-operant">{currentOperand}</div>
           </div>
-          <button className="span-two">AC</button>
+          <button
+            className="span-two"
+            onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+          >
+            AC
+          </button>
           <button>DEL</button>
           <OperationButton operation="/" dispatch={dispatch} />
           <DigitButton digit="1" dispatch={dispatch} />
           <DigitButton digit="2" dispatch={dispatch} />
           <DigitButton digit="3" dispatch={dispatch} />
-          <OperationButton peration="*" dispatch={dispatch} />
+          <OperationButton operation="*" dispatch={dispatch} />
           <DigitButton digit="4" dispatch={dispatch} />
           <DigitButton digit="5" dispatch={dispatch} />
           <DigitButton digit="6" dispatch={dispatch} />
-          <OperationButton peration="+" dispatch={dispatch} />
+          <OperationButton operation="+" dispatch={dispatch} />
           <DigitButton digit="7" dispatch={dispatch} />
           <DigitButton digit="8" dispatch={dispatch} />
           <DigitButton digit="9" dispatch={dispatch} />
-          <OperationButton peration="-" dispatch={dispatch} />
+          <OperationButton operation="-" dispatch={dispatch} />
           <DigitButton digit="." dispatch={dispatch} />
           <DigitButton digit="0" dispatch={dispatch} />
           <button className="span-two">=</button>
