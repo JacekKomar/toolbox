53a5a818031776b6bd8ce9915f096ee661d7cf42
JacekKomar <jacek.komar2000@gmail.com>
2022-12-07 12:02:29 +0100

    Add option writing noumbers in calculator

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
-import "../../css/calculator.scss";
 import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+import "../../css/calculator.scss";
 
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
@@ -13,22 +14,24 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
       return {
         ...state,
-        currentOperand: `${currentOperand || ""}  ${payload.digit}`,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
   }
 }
 
-// export default class Calculator extends React.Component
-
-//   render() {
 function Calculator() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
     {}
   );
-  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
 
   return (
     <div className="container">
@@ -47,27 +50,26 @@ function Calculator() {
           </div>
           <button className="span-two">AC</button>
           <button>DEL</button>
-          <DigitButton digit="/" dispatch={dispatch} />
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>*</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>-</button>
-          <button>.</button>
-          <button>0</button>
+          <OperationButton operation="/" dispatch={dispatch} />
+          <DigitButton digit="1" dispatch={dispatch} />
+          <DigitButton digit="2" dispatch={dispatch} />
+          <DigitButton digit="3" dispatch={dispatch} />
+          <OperationButton peration="*" dispatch={dispatch} />
+          <DigitButton digit="4" dispatch={dispatch} />
+          <DigitButton digit="5" dispatch={dispatch} />
+          <DigitButton digit="6" dispatch={dispatch} />
+          <OperationButton peration="+" dispatch={dispatch} />
+          <DigitButton digit="7" dispatch={dispatch} />
+          <DigitButton digit="8" dispatch={dispatch} />
+          <DigitButton digit="9" dispatch={dispatch} />
+          <OperationButton peration="-" dispatch={dispatch} />
+          <DigitButton digit="." dispatch={dispatch} />
+          <DigitButton digit="0" dispatch={dispatch} />
           <button className="span-two">=</button>
         </div>
       </div>
     </div>
   );
 }
-//   }
-// }
+
 export default Calculator;
