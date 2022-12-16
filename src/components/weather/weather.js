f233ce7bb82ce1abd8212aa1a41c15d6ee57d9bd
JacekKomar <jacek.komar2000@gmail.com>
2022-12-16 17:15:34 +0100

    Add json files to weather app

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/weather.scss";
 import cityIcon from "./weatherImages/cityIcon.png";
+import axios from "axios";
 
 export default function Weather() {
   const [mojText, ustawText] = React.useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
 
   return (
     <div className="container mb-5">
