92fe5c4df77c826fae2ed6085fc88393e8b02f1c
JacekKomar <jacek.komar2000@gmail.com>
2022-12-16 20:38:05 +0100

    Add State in weather

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import "../../css/weather.scss";
 import cityIcon from "./weatherImages/cityIcon.png";
-import axios from "axios";
+// import axios from "axios";
 
 export default function Weather() {
   const [mojText, ustawText] = React.useState("");
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
 
   return (
     <div className="container mb-5">
