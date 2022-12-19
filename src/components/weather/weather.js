88bafb5befd1c4f15f6d9c9fcb1fb4769fd13208
JacekKomar <jacek.komar2000@gmail.com>
2022-12-19 13:15:11 +0100

    Add buttons to weather

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -21,6 +21,21 @@ function Weather() {
 
   return (
     <div className="container mb-5">
+      <div className="weatherTable rounded border border-dark mb-2 weatherButtons">
+        <button className="col-3 weatherButton  rounded border border-dark brightenedDark">
+          <h1> OpenWeather </h1>
+        </button>
+        <button className="col-3 weatherButton  rounded border border-dark brightenedDark">
+          <h1> Weather Api </h1>
+        </button>
+        <button className="col-3 weatherButton  rounded border border-dark brightenedDark">
+          <h1> IMGW </h1>
+        </button>
+        <button className="col-3 weatherButton  rounded border border-dark brightenedDark">
+          <h1> Open Meteo </h1>
+        </button>
+      </div>
+
       <h1>
         <input
           value={location}
