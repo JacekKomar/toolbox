5305d40b05b6a995c1a8196b864fdb8afc688afd
JacekKomar <jacek.komar2000@gmail.com>
2022-12-23 13:23:50 +0100

    Fixed citation table

--- src/components/Header/Citation.js
+++ src/components/Header/Citation.js
@@ -2,46 +2,17 @@ import React from "react";
 
 function RenderingCitation() {
   const quotes = [
-    {
-      quote: "Ludzie boją się zmian, nawet na lepsze.",
-      author: "J. I. Kraszewski",
-    },
-    {
-      quote:
-        "Ludzie, którzy tracą czas czekając, aż zaistnieją najbardziej sprzyjające warunki, nigdy nic nie zdziałają. Najlepszy czas na działanie jest teraz!.",
-      author: "Mark Fisher",
-    },
-    {
-      quote:
-        "W życiu nie chodzi o czekanie, aż burza minie… Chodzi o to, by nauczyć się tańczyć w deszczu.",
-      author: "Vivian Green",
-    },
-    {
-      quote:
-        "Sposobem na zaczęcie jest skończenie mówienia i podjęcie działania.",
-      author: "Walt Disney",
-    },
-    {
-      quote:
-        "Żadne zadanie nie jest szczególnie trudne, jeśli podzielisz je na mniejsze podzadania.",
-      author: "Henry Ford",
-    },
-    {
-      quote:
-        "Pamiętaj, że kiedy potrzebujesz pomocnej dłoni – jest ona na końcu twojego ramienia.",
-      author: "Audrey Hepburn",
-    },
-    {
-      quote:
-        "Tylko ci, którzy ryzykują pójście za daleko dowiedzą się jak daleko można dojść.",
-      author: "T.S. Eliot",
-    },
-    {
-      quote:
-        "Umysł potrzebuje zaś książek, podobnie jak miecz potrzebuje kamienia do ostrzenia.",
-      author: "George R.R. Martin",
-    },
+    "Ludzie boją się zmian, nawet na lepsze- J. I. Kraszewski",
+    "Ludzie, którzy tracą czas czekając, aż zaistnieją najbardziej sprzyjające warunki, nigdy nic nie zdziałają. Najlepszy czas na działanie jest teraz! - Mark Fisher",
+    "W życiu nie chodzi o czekanie, aż burza minie… Chodzi o to, by nauczyć się tańczyć w deszczu. - Vivian Green",
+    "Sposobem na zaczęcie jest skończenie mówienia i podjęcie działania. - Walt Disney",
+    "Żadne zadanie nie jest szczególnie trudne, jeśli podzielisz je na mniejsze podzadania. - Henry Ford",
+    "Pamiętaj, że kiedy potrzebujesz pomocnej dłoni jest ona na końcu twojego ramienia. - Audrey Hepburn",
+    "Tylko ci, którzy ryzykują pójście za daleko dowiedzą się jak daleko można dojść. - T.S. Eliot",
+    "Umysł potrzebuje zaś książek, podobnie jak miecz potrzebuje kamienia do ostrzenia. - George R.R. Martin",
   ];
+  const quoteShow = quotes[Math.floor(Math.random() * quotes.length)];
+  console.log(`${quoteShow}`);
 
   const OneQuote = quotes.map((element) => {
     return (
