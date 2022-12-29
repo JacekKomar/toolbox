b76af797879131a8e3de05cac9fc53bf884d330e
JacekKomar <jacek.komar2000@gmail.com>
2022-12-29 17:28:18 +0100

    Edited DayPlanForm component

--- src/components/dayPlan/DayPlan.js
+++ src/components/dayPlan/DayPlan.js
@@ -1,25 +1,18 @@
 import React from "react";
 import "../../css/dayPlan.scss";
-// to jest komponent funkcyjny, czyli że zamiast nazwy klasy robimy funkcję
+import DayPlanForm from "./DayPlanForm";
+
 export default function DayPlan() {
-  const [mojText, ustawText] = React.useState(""); // to jest Hook useState
-  console.log(mojText);
   return (
     <div className="container">
       <div className="dayPlanTable rounded border border-dark text-center">
         <h1>Twój plan dnia:</h1>
       </div>
       <h1>
-        <input
-          type="text"
-          onChange={(event) => ustawText(event.target.value)}
-          className="dayPlanTable rounded border border-dark text-center mt-2 col-12"
-          placeholder="  Wpisz nazwę czynności:"
-        ></input>
+        <div className="dayPlanTable rounded border border-dark text-center mt-2 col-12">
+          <DayPlanForm />
+        </div>
       </h1>
-      <div className="dayPlanTable rounded border border-dark mt-2 text-center">
-        <div>{mojText}</div>
-      </div>
     </div>
   );
 }
