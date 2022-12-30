56216bca440f8e7256790f73f2d8c96d09ef128d
JacekKomar <jacek.komar2000@gmail.com>
2022-12-30 11:57:36 +0100

    Fixed rounded of buttons in calculator

--- src/components/calculator/OperationButton.js
+++ src/components/calculator/OperationButton.js
@@ -1,8 +1,9 @@
 import { ACTIONS } from "./Calculator";
 
-export default function OperationButton({ dispatch, operation }) {
+export default function OperationButton({ dispatch, operation, className }) {
   return (
     <button
+      className={className}
       onClick={() =>
         dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
       }
